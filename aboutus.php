65d271d7e5c5a8d56e7d21cd1897bcbfa5724860
AlexWolak <alexandrewolak@gmail.com>
2024-01-14 11:58:29 +0100

    update about us file

--- aboutus.php
+++ aboutus.php
@@ -1,27 +1,38 @@
 <!DOCTYPE html>
+<?php
+include 'includes/languages.php';
+?>
 <html>
 <!-- header -->
 <head>
-    <title>About GamePulse</title>
+    <title><?php echo $about_home[$langue]; ?></title>
     <!-- Include Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Include your custom stylesheet -->
     <link href="stylesheet.css" rel="stylesheet">
+    <style>
+        .drapeau {
+            width: 40px;
+        }
+
+    </style>
 </head>
 
 <body>
     <!-- Navbar (if applicable) -->
     <!-- Include Navbar from another file or code it here -->
-
+    <?php
+        include 'includes/header.inc.php';       // Include header
+    ?>
     <!-- Main Content -->
     <div class="container mt-5">
         <div class="row">
             <div class="col-md-8 offset-md-2 text-center">
-                <h1>About GamePulse</h1>
-                <h2 class="lead">GamePulse is more than just a platform; it's a vibrant community that celebrates gaming in all its forms.</h2>
-                <p>At GamePulse, our mission is to provide gamers with a curated space where they can discover, explore, and engage with a diverse array of games. We believe that gaming is not just a pastime but an immersive experience that brings people together, sparks creativity, and offers endless entertainment.</p>
-                <p>Our team of passionate developers, designers, and gaming enthusiasts works tirelessly to ensure that GamePulse remains a hub for all things gaming. From showcasing popular titles to highlighting upcoming releases, we strive to create an inclusive environment that caters to gamers of every taste and preference.</p>
-                <p>Join us on this journey as we continue to evolve and grow. GamePulse is your destination to stay connected with the pulse of gaming excitement.</p>
+                <h1><?php echo $about_home[$langue]; ?></h1>
+                <h2 class="lead"><?php echo $about1[$langue]; ?></h2>
+                <p><?php echo $about2[$langue]; ?></p>
+                <p><?php echo $about3[$langue]; ?></p>
+                <p><?php echo $about4[$langue]; ?></p>
             </div>
         </div>
     </div>
