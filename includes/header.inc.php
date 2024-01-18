c0d024d891b6c00193386aea32d6066d5765a17d
Donovan Townes <86418248+Z8phyR@users.noreply.github.com>
2024-01-18 14:28:07 -0600

    Update Navbar responsiveness

--- includes/header.inc.php
+++ includes/header.inc.php
@@ -15,30 +15,7 @@ function isPageActive($pageName) {
 }
 ?>
 
-<header class="header bg-dark text-light py-3 border-bottom">
-    <div class="d-flex justify-content-between px-4">
-        <a href="index.php" class="fs-4 text-light text-decoration-none">Game Pulse</a>
-        <ul class="nav nav-pills">
-            <li class="nav-item">
-                <a href="index.php" class="nav-link <?php echo (isPageActive('index.php')) ? 'active' : ''; ?>" aria-current="page">
-                    <?php echo $home[$langue]; ?>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href="game_list.php" class="nav-link <?php echo (isPageActive('game_list.php')) ? 'active' : ''; ?>">
-                    <?php echo $game_list[$langue]; ?>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href ="<?php echo $_SERVER['PHP_SELF']; ?>?lang=1" class="nav-link" target="_self">
-                    <img src="images/drapeau-francais.jpg" class="drapeau"/>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a href ="<?php echo $_SERVER['PHP_SELF']; ?>" target="_self" class="nav-link">
-                    <img src="images/drapeau-anglais.jpg" class="drapeau"/>
-                </a>
-            </li>  
-        </ul>
-    </div>
+<header class="header bg-dark text-light py-3">
+    <?php include 'includes/navbar.php'; ?>
 </header>
+
---
+++ includes/navbar.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-dark">
+
+<a href="index.php" class="navbar-brand px-2">Game Pulse</a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse justify-content-end py-1" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a href="index.php" class="d-flex justify-content-end py-2 nav-link <?php echo (isPageActive('index.php')) ? 'active' : ''; ?>">
+                <?php echo $home[$langue]; ?>
+            </a>
+        </li>
+        <li class="nav-item">
+    <a href="game_list.php" class="d-flex justify-content-end py-2 nav-link <?php echo (isPageActive('game_list.php')) ? 'active' : ''; ?>">
+        <?php echo $game_list[$langue]; ?>
+    </a>
+</li>
+<li class="nav-item">
+    <a href ="<?php echo $_SERVER['PHP_SELF']; ?>?lang=1" class="d-flex justify-content-end py-2 nav-link" target="_self">
+        <img src="images/drapeau-francais.jpg" class="drapeau"/>
+    </a>
+</li>
+<li class="nav-item">
+    <a href ="<?php echo $_SERVER['PHP_SELF']; ?>" target="_self" class="d-flex justify-content-end py-2 nav-link">
+        <img src="images/drapeau-anglais.jpg" class="drapeau"/>
+    </a>
+    </ul>
+</div>
+</nav>
