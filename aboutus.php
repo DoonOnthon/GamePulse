70fc3ba2b4ebea927be0b1526ace87c6a2154eaa
Edzer Dionido <jeddionido26@gmail.com>
2024-02-13 19:12:25 +0800

    rename css file

--- aboutus.php
+++ aboutus.php
@@ -9,7 +9,7 @@ include 'includes/datas/languages.php';
     <!-- Include Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Include your custom stylesheet -->
-    <link href="assets/css/stylesheet.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
 </head>
 
 <body>
