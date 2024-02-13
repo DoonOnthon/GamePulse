cdd1070dd65216abecfb76d6fcb04f6141c80220
Edzer Dionido <jeddionido26@gmail.com>
2024-02-13 17:51:52 +0800

    put games and language data here

---
+++ includes/datas/games_data.php
@@ -0,0 +1,424 @@
+<?php
+// Sample game data
+$games = [
+    [
+        'title' => 'The Last Guardian',
+        'category' => 'Action-Adventure',
+        'release_date' => '2016-12-06',
+        'sales_numbers' => '2,000,000',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/4cDuKShhQOA?si=GGnzRIIrznF-35-n'
+    ],
+    [
+        'title' => 'Dark and Darker',
+        'category' => 'Stealth Action',
+        'release_date' => '2023-04-15',
+        'sales_numbers' => '1,500,000',
+        'github_username' => 'Doononthon',
+        'youtube_trailer' => 'https://youtu.be/vKZll_JKBvk?si=lkmQNml7ehaIXsrB'
+    ],
+    [
+        'title' => 'The Elder Scrolls V: Skyrim',
+        'category' => 'RPG',
+        'release_date' => '2011-11-11',
+        'sales_numbers' => '30,000,000+',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/JSRtYpNRoN0?si=hMTtiK8kvR8xxxkc'
+    ],
+    [
+        'title' => 'Overwatch',
+        'category' => 'First-person shooter',
+        'release_date' => '2016-05-24',
+        'sales_numbers' => '50,000,000+',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/dushZybUYnM?si=wLbTYZXF974zbYgA'
+        
+    ],
+    [
+        'title' => 'Assassin\'s Creed Valhalla',
+        'category' => 'Adventure',
+        'release_date' => '2020-11-10',
+        'sales_numbers' => '14,000,000',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/ssrNcwxALS4?si=wZsHxEr8COsGgm3R'
+    ],
+    [
+        'title' => 'The Witcher 3: Wild Hunt',
+        'category' => 'RPG',
+        'release_date' => '2015-05-19',
+        'sales_numbers' => '30,000,000',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/XHrskkHf958?si=3mZB4P0WIcst_I4E'
+    ],
+    [
+        'title' => 'The Legend of Zelda: Breath of the Wild',
+        'category' => 'Action-Adventure',
+        'release_date' => '2017-03-03',
+        'sales_numbers' => '22,000,000',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/zw47_q9wbBE?si=1WeOiN2rrcHqqU4j'
+        
+    ],
+    [
+        'title' => 'Minecraft',
+        'category' => 'Sandbox',
+        'release_date' => '2011-11-18',
+        'sales_numbers' => '238,000,000',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/MmB9b5njVbA?si=DF8iQGAIboOcyVER'
+    ],
+    [
+        'title' => 'Fortnite',
+        'category' => 'Battle Royale',
+        'release_date' => '2017-07-25',
+        'sales_numbers' => '350,000,000',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/WJW-bzXZM8M?si=YPjHOr0MSpRZBrMS'
+    ],
+    [
+        'title' => 'Among Us',
+        'category' => 'Social Deduction',
+        'release_date' => '2018-06-15',
+        'sales_numbers' => '500,000,000',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => ''
+    ],
+    [
+        'title' => 'Red Dead Redemption 2',
+        'category' => 'Action-Adventure',
+        'release_date' => '2018-10-26',
+        'sales_numbers' => '38,000,000',
+        'github_username' => 'LilMaddy',
+        'youtube_trailer' => 'https://youtu.be/gmA6MrX81z4?si=cuOASskCKMPoJxhj'
+    ],
+    [
+        'title' => 'Horizon Zero Dawn',
+        'category' => 'Action-Adventure',
+        'release_date' => '2017-02-28',
+        'sales_numbers' => '10,000,000',
+        'github_username' => 'Legen32',
+        'youtube_trailer' => 'https://youtu.be/lwI6-jKlsO0?si=1LkRtwkalDvYgwlD'
+    ],
+    [
+        'title' => 'Grand Theft Auto V',
+        'category' => 'action-adventure',
+        'release_date' => '2013-09-17',
+        'sales_numbers' => '140,000,000',
+        'github_username' => 'tetawiah',
+        'youtube_trailer' => 'https://youtu.be/QkkoHAzjnUs?si=RMlggjzkAhgMm8E0'
+    ],
+    [
+        'title' => 'Tetris',
+        'category' => 'Puzzle',
+        'release_date' => '1984-06-06',
+        'sales_numbers' => '170,000,000',
+        'github_username' => 'tetawiah',
+        'youtube_trailer' => ''
+    ],
+    [
+        'title' => 'Call of Duty: Modern Warfare 2',
+        'category' => 'First-person shooter',
+        'release_date' => '2009-11-10',
+        'sales_numbers' => '22,700,000',
+        'github_username' => 'tetawiah',
+        'youtube_trailer' => 'https://youtu.be/blshENwbHgs?si=RvCfdjxmnu3ZN_cq'
+    ],
+    [
+        'title' => 'Elden Ring',
+        'category' => 'ARPG',
+        'release_date' => '2022-02-25',
+        'sales_numbers' => '20,500,000',
+        'github_username' => 'harrydemorgan',
+        'youtube_trailer' => 'https://youtu.be/E3Huy2cdih0?si=QUqdQKV1iAQ7oOem'
+    ],
+    [
+        'title' => 'Counter-Strike: Global Offensive',
+        'category' => 'First-person shooter',
+        'release_date' => '2012-08-21',
+        'sales_numbers' => '45,000,000+',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/edYCtaNueQY?si=DYgPJi5VcqoaAbvq'
+    ],
+    [
+        'title' => 'Beat Saber',
+        'category' => 'Rhythm',
+        'release_date' => '2018-05-01',
+        'sales_numbers' => '6,000,000+',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/vL39Sg2AqWg?si=BxSVUNGlBe0Obfsp'
+    ],
+    [
+        'title' => 'Resident Evil 8: Village',
+        'category' => 'Action-Adventure',
+        'release_date' => '2021-05-07',
+        'sales_numbers' => '8,000,000+',
+        'github_username' => 'Omanshu209',
+        'youtube_trailer' => 'https://youtu.be/QKocYiRHYlI?si=S0O2Ebj2HBaQcUn8'
+    ],
+    [
+        'title' => 'Mount & Blade II: Bannerlord',
+        'category' => 'RPG',
+        'release_date' => '2022-10-25',
+        'sales_numbers' => '3,100,000+',
+        'github_username' => 'AlandisAyupov',
+        'youtube_trailer' => 'https://youtu.be/q6oH5cW7PaA?si=N8ErmmYQkhik2Jni'
+    ],
+    [
+        'title' => 'Cyberpunk 2077',
+        'category' => 'Action-Adventure',
+        'release_date' => '2020-12-10',
+        'sales_numbers' => '20,000,000+',
+        'github_username' => 'Camarota-234',
+        'youtube_trailer' => 'https://youtu.be/P99qJGrPNLs?si=qWBJiwM9YiM2n6aG'
+    ],
+    [
+        'title' => 'God of War Ragnarök',
+        'category' => 'Action-Adventure',
+        'release_date' => '2022-11-09',
+        'sales_numbers' => '11,000,000+',
+        'github_username' => 'Camarota-234',
+        'youtube_trailer' => 'https://youtu.be/EE-4GvjKcfs?si=dr7amhqzGZdbmcYk'
+    ],
+    [
+        'title' => 'The Last of Us 2',
+        'category' => 'Action-Adventure',
+        'release_date' => '2020-19-06',
+        'sales_numbers' => '10,000,000+',
+        'github_username' => 'Camarota-234',
+        'youtube_trailer' => 'https://youtu.be/qPNiIeKMHyg?si=Eboyxfo0noDkdKtD'
+    ],
+    [
+        'title' => 'Phasmophobia',
+        'category' => 'Adventure',
+        'release_date' => '2020-9-18',
+        'sales_numbers' => '5,000,000+',
+        'github_username' => 'Omanshu209',
+        'youtube_trailer' => 'https://youtu.be/sRa9oeo5KiY?si=3fEwnQBdImft3lhG'
+    ],
+    [
+        'title' => 'Infliction',
+        'category' => 'Adventure',
+        'release_date' => '2018-10-17',
+        'sales_numbers' => '4,000,000+',
+        'github_username' => 'Omanshu209',
+        'youtube_trailer' => ''
+    ],
+    [
+        'title' => 'MADiSON',
+        'category' => 'Adventure',
+        'release_date' => '2022-07-08',
+        'sales_numbers' => '3,000,000+',
+        'github_username' => 'Omanshu209',
+        'youtube_trailer' => 'https://youtu.be/Il_TFl_vPoo?si=pU6-dcsvz4Xbmf-v'
+    ],
+    [
+        'title' => 'Mount & Blade: Warband',
+        'category' => 'Action RPG',
+        'release_date' => '2010-03-30',
+        'sales_numbers' => '6,000,000+',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/hwGxzdGqEDg?si=MXfVo49M1-bXLH90'
+    ],
+    [
+        'title' => 'Pummel Party',
+        'category' => 'Party Game',
+        'release_date' => '2018-09-20',
+        'sales_numbers' => 'Over 1,000,000 on Steam',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/oiYG0Ov5jKE?si=a1A_NorgQkWtrdRX'
+    ],
+    [
+        'title' => "Tom Clancy's Rainbow Six Siege",
+        'category' => 'First-person shooter, Action, Strategy',
+        'release_date' => '2015-12-01',
+        'sales_numbers' => '36,000,000+',
+        'github_username' => 'lucas-dantas10',
+        'youtube_trailer' => 'https://youtu.be/wV8QCvZXmZQ?si=yc4I57R0zxPUoYE0'
+    ],
+    [
+        'title' => 'Rocket League',
+        'category' => 'Electronic Sport Game, Action',
+        'release_date' => '2015-07-07',
+        'sales_numbers' => '10,000,000+',
+        'github_username' => 'lucas-dantas10',
+        'youtube_trailer' => 'https://youtu.be/SgSX3gOrj60?si=A1v9PMTCS09g2kaW'
+    ],
+    [
+        'title' => 'Hearthstone',
+        'category' => 'Cards Game, Strategy',
+        'release_date' => '2014-03-11',
+        'sales_numbers' => '5,000,000+',
+        'github_username' => 'lucas-dantas10',
+        'youtube_trailer' => ''
+    ],
+    [
+        'title' => 'New World',
+        'category' => 'MMORPG, Action-Adventure',
+        'release_date' => '2021-09-28',
+        'sales_numbers' => '700,000+',
+        'github_username' => 'lucas-dantas10',
+        'youtube_trailer' => 'https://youtu.be/m1C5dLnvlzg?si=zXaKkDj4d7nVDY-o'
+    ],
+    [
+        'title' => 'Kingdom Hearts',
+        'category' => 'Action RPG',
+        'release_date' => '2002-03-28',
+        'sales_numbers' => '100,000,000+',
+        'github_username' => 'Emma-Jonna',
+        'youtube_trailer' => 'https://youtu.be/CzgrMWisdU0?si=nDyBCyoLhxbQ51nr'
+    ],
+    [
+        'title' => 'Untitled Goose Game',
+        'category' => 'Puzzle, Stealth',
+        'release_date' => '2019-09-20',
+        'sales_numbers' => '5,000,000+',
+        'github_username' => 'Emma-Jonna',
+        'youtube_trailer' => 'https://youtu.be/9LL2AtHo1gk?si=ZWPzWl4CQOvWpK09'
+    ],
+    [
+        'title' => 'Ōkami',
+        'category' => 'Action-Adventure',
+        'release_date' => '2006-04-20',
+        'sales_numbers' => '4,000,000+',
+        'github_username' => 'Emma-Jonna',
+        'youtube_trailer' => 'https://youtu.be/RhnhRfTzRnw?si=-cekAx2tHem3FzdO'
+    ],
+    [
+        'title' => 'Super Mario Odyssey',
+        'category' => 'Platformer',
+        'release_date' => '2017-10-27',
+        'sales_numbers' => '20,230,000',
+        'github_username' => 'adesh1998',
+        'youtube_trailer' => 'https://youtu.be/u6oPBIVjf8E?si=DMi5-Zv5M3VnVRBD'
+    ],
+    [
+        'title' => 'Lethal Company',
+        'category' => 'First-person Shooter',
+        'release_date' => '2023-06-15',
+        'sales_numbers' => '12,500,000',
+        'github_username' => 'DoonOnthon',
+        'youtube_trailer' => 'https://youtu.be/Su6OsTb1w9Q?si=bvBFc0vy9rfBR0NV'
+    ],
+    [
+        'title' => 'Hogwarts Legacy',
+        'category' => 'Action RPG',
+        'release_date' => '2023-02-10',
+        'sales_numbers' => '15,000,000',
+        'github_username' => 'Camarota-234',
+        'youtube_trailer' => 'https://youtu.be/BtyBjOW8sGY?si=cqs2MUSJP3PhTUPd'
+    ],
+    [
+        'title' => 'Star Wars Jedi: Fallen Order',
+        'category' => 'Action-Adventure',
+        'release_date' => '2019-11-15',
+        'sales_numbers' => '10,000,000',
+        'github_username' => 'Camarota-234',
+        'youtube_trailer' => 'https://youtu.be/0GLbwkfhYZk?si=MzQYmNfO-BC58b7_'
+    ],
+    [
+        'title' => 'Star Wars Jedi: Survivor',
+        'category' => 'Action-Adventure',
+        'release_date' => '2023-04-18',
+        'sales_numbers' => '10,400,000',
+        'github_username' => 'Camarota-234',
+        'youtube_trailer' => 'https://youtu.be/VRaobDJjiec?si=0cnwcDnJhDVFXOVs'
+    ],
+    [
+        'title' => 'Baldurs Gate 3',
+        'category' => 'RPG',
+        'release_date' => '2023-08-03',
+        'sales_numbers' => '5,200,000',
+        'github_username' => 'Camarota-234',
+        'youtube_trailer' => 'https://youtu.be/4YH9HPQ2xPg?si=PvjkqYjcV18cuUHs'
+    ],
+    [
+        'title' => 'Marvels Spider-Man',
+        'category' => 'Action-Adventure',
+        'release_date' => '2018-09-07',
+        'sales_numbers' => '20,000,000',
+        'github_username' => 'Camarota-234',
+        'youtube_trailer' => 'https://youtu.be/bbFh-Hbd81o?si=ZEUTsjBM-cxCIg2V'
+    ],
+    [
+        'title' => 'Spider-Man: Miles Morales',
+        'category' => 'Action-Adventure',
+        'release_date' => '2020-11-11',
+        'sales_numbers' => '4,100,000',
+        'github_username' => 'Camarota-234',
+        'youtube_trailer' => 'https://youtu.be/NTunTURbyUU?si=Q1ejrBZsG3HfLTDk'
+    ],
+    [
+        'title' => 'PUBG Battlegrounds',
+        'category' => 'Battle Royale',
+        'release_date' => '2017-12-20',
+        'sales_numbers' => '75,000,000',
+        'github_username' => 'dalek63',
+        'youtube_trailer' => 'https://youtu.be/S-6TBeruY6w?si=eUjkz2e3RYsVVA9n'
+    ],
+    [
+        'title' => 'MarioKart 8 Deluxe',
+        'category' => 'Racing',
+        'release_date' => '2017-04-28',
+        'sales_numbers' => '65,570,000',
+        'github_username' => 'dalek63',
+        'youtube_trailer' => 'https://youtu.be/tKlRN2YpxRE?si=Z7z6eCNKMDKKazRQ'
+    ],
+    [
+        'title' => 'Wii Sports',
+        'category' => 'Sport',
+        'release_date' => '2006-11-19',
+        'sales_numbers' => '82,900,000',
+        'github_username' => 'dalek63',
+        'youtube_trailer' => ''
+    ],
+    [
+        'title' => 'Human: Fall Flat',
+        'category' => 'Platformer',
+        'release_date' => '2016-07-22',
+        'sales_numbers' => '50,000,000',
+        'github_username' => 'dalek63',
+        'youtube_trailer' => 'https://youtu.be/jNV4lCNEp_0?si=7vQPKct40TLNw0yM'
+    ],
+    [
+        'title' => 'Terraria',
+        'category' => 'Sandbox',
+        'release_date' => '2011-05-16',
+        'sales_numbers' => '44,500,000',
+        'github_username' => 'dalek63',
+        'youtube_trailer' => 'https://youtu.be/w7uOhFTrrq0?si=0Fp0x3Iw4gM2nWek'
+    ],
+    [
+        'title' => 'Call of Duty: Modern Warfare 3',
+        'category' => 'First-person Shooter',
+        'release_date' => '2011-11-08',
+        'sales_numbers' => '26,500,000',
+        'github_username' => 'dalek63',
+        'youtube_trailer' => 'https://youtu.be/coiTJbr9m04?si=kTgKUCAXX66GbURv'
+    ],
+    [
+        'title' => 'Palworld',
+        'category' => 'RPG',
+        'release_date' => '2024-01-19',
+        'sales_numbers' => '8,000,000+',
+        'github_username' => 'HassanMirza123',
+        'youtube_trailer' => 'https://www.youtube.com/watch?v=ZPu9yAOEYkQ'
+    ],
+    [
+        'title' => 'Tekken 8',
+        'category' => 'Fighting',
+        'release_date' => '2024-01-26',
+        'sales_numbers' => '1,000,000+',
+        'github_username' => 'HassanMirza123',
+        'youtube_trailer' => 'https://www.youtube.com/watch?v=_MM4clV2qjE'
+    ],
+    [
+        'title' => 'EA Sports UFC 5',
+        'category' => 'Fighting',
+        'release_date' => '2023-10-24',
+        'sales_numbers' => '1,000,000+',
+        'github_username' => 'HassanMirza123',
+        'youtube_trailer' => 'https://www.youtube.com/watch?v=1R4MdobS1nI'
+    ],
+    
+
+];
---
+++ includes/datas/languages.php
@@ -0,0 +1,54 @@
+<?php
+
+$langue=0;
+if(isset($_GET["lang"]))
+    $langue=1;
+
+//game_list.php
+$titre1 = array("Welcome to GamePulse!", "Bienvenue sur GamePulse!");
+$titre2 = array("Explore the world of gaming and find your next adventure.", "Explorez le monde du jeu et trouvez votre prochaine aventure.");
+$filter1 = array("Filter by title:","Filtrer par titre:");
+$filter2 = array("Filter by catecogry:","Filtrer par catégorie:");
+$filter3 = array("Filter by contributor:","Filtrer par contributeur:");
+$reset_search = array("Reset Search","Réinitialiser la recherche");
+$search_bar = array("Search for games by title, category, or release date","Rechercher des jeux par titre, catégorie ou date de sortie");
+$search_btn = array("Search","Rechercher");
+$game_title = array("Game Title","Titre du Jeu");
+$game_category = array("Category","Catégorie");
+$game_date = array("Release Date","Date de sortie");
+$game_sales_number = array("Sales Numbers (Approx)","Nombre de ventes (Approx)");
+$game_contributor = array("Contributor","Contributeur");
+$game_trailer = array("Trailer","Bande d'annonce");
+$game_actions = array("Actions","Options");
+$game_trailer_btn = array("Watch Trailer","Regarder la Bande d'annonce");
+$game_details = array("Details","Details");
+$select_title = array("Choose Titles","Choisir Titres");
+$select_category = array("Choose Categories","Choisir Catégories");
+$select_contributor = array("Choose Contributors","Choisir Contributeurs");
+
+//header
+$home = array("Home","Accueil");
+$game_list = array("Game List","Liste des Jeux");
+$about_us = array("About Us","À propos de nous");
+$confidentiality = array("Privacy Policy","Politique de confidentialité");
+$follow_us = array("Follow Us","Suivez-nous");
+$contact_us = array("Contact Us","Contactez-nous");
+$contact_us_2 = array("For inquiries and support, email us at :","Pour obtenir des informations ou de l'aide, écrivez-nous à :");
+$rights = array("© 2023 GamePulse. All rights reserved.","© 2023 GamePulse. Tous droits réservés.");
+
+
+$titre2_home = array("Explore the world of gaming and find your next adventure.","Explorez le monde des jeux vidéos et trouvez votre prochaine aventure.");
+$btn_game_list = array("View Game List","Voir la liste des jeux");
+$about_home = array("About GamePulse","À propos de GamePulse");
+$desc1 = array("GamePulse is your gateway to discovering an exhilarating realm of video games. With a handpicked selection spanning diverse genres, GamePulse empowers gamers to explore, experience, and engage with captivating titles.","GamePulse est votre porte d’entrée pour découvrir un royaume exaltant du gaming. Avec une fine sélection couvrant divers genres, GamePulse permet aux joueurs d’explorer, d’expérimenter et d’interagir avec des titres captivants.");
+$desc2 = array("Whether you're on the lookout for high-octane action, mind-bending puzzles, or immersive narratives, our platform curates an exceptional collection that resonates with gaming enthusiasts of all levels.","Que vous soyez à la recherche d’action, de puzzles époustouflants ou de récits immersifs, notre plateforme propose une collection exceptionnelle qui résonne avec les amateurs de gaming de tous niveaux.");
+$desc3 = array("Join us on this journey as we celebrate the art, innovation, and endless enjoyment that the world of gaming offers. Fuel your passion, ignite your curiosity, and uncover your next unforgettable gaming experience with GamePulse.","Rejoignez-nous pour célébrer l’art, l’innovation et le plaisir sans fin que le monde du gaming offre. Alimentez votre passion, éveillez votre curiosité et découvrez votre prochaine expérience de jeu inoubliable avec GamePulse.");
+
+$popular_games = array("Popular Games Right Now","Jeux populaires en ce moment");
+$upcoming_games = array("Great Upcoming Games!","Bons jeux à venir!");
+
+$about1 = array("GamePulse is more than just a platform; it's a vibrant community that celebrates gaming in all its forms.","GamePulse est plus qu’une simple plateforme; c’est une communauté dynamique qui célèbre le gaming sous toutes ses formes.");
+$about2 = array("At GamePulse, our mission is to provide gamers with a curated space where they can discover, explore, and engage with a diverse array of games. We believe that gaming is not just a pastime but an immersive experience that brings people together, sparks creativity, and offers endless entertainment.","Chez GamePulse, notre mission est de fournir aux joueurs un espace organisé où ils peuvent découvrir, explorer et interagir avec un large éventail de jeux vidéos. Nous savons que le jeu n’est pas seulement un passe-temps, mais une expérience immersive qui rassemble les gens, stimule la créativité et offre un divertissement sans fin.");
+$about3 = array("Our team of passionate developers, designers, and gaming enthusiasts works tirelessly to ensure that GamePulse remains a hub for all things gaming. From showcasing popular titles to highlighting upcoming releases, we strive to create an inclusive environment that caters to gamers of every taste and preference.", "Notre équipe de développeurs, de designers et de passionnés de gaming travaille sans relâche pour que GamePulse reste une plaque tournante du gaming. De la présentation de titres populaires à la mise en valeur des versions à venir, nous nous efforçons de créer un environnement inclusif qui s’adresse aux joueurs de tous les goûts et préférences.");
+$about4 = array("Join us on this journey as we continue to evolve and grow. GamePulse is your destination to stay connected with the pulse of gaming excitement.", "Rejoignez-nous dans cette aventure alors que nous continuons d’évoluer et de grandir. GamePulse est votre destination pour rester connecté avec l'émotion du gaming.");
+?>
